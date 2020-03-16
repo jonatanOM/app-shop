2bc6e053398c02dd7ffa04fb7bfd037be5f9f0d8
jonatanOM <xyz_1232@hotmail.com>
2020-03-16 16:27:31 -0400

    Base project

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TestController@welcome');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
